1af855042a05778d029d420b2a4bc1b9a0c09f30
Sasha Sax <sax@berkeley.edu>
2024-01-01 10:26:56 -0800

    zenodo hosting

--- omnidata_tools/torch/tools/download_depth_models.sh
+++ omnidata_tools/torch/tools/download_depth_models.sh
@@ -4,18 +4,23 @@
 # unzip -j download -d pretrained_models
 # rm download
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get install apt-transport-https ca-certificates gnupg
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update -y && sudo apt-get install -y google-cloud-sdk
+# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# sudo apt-get install apt-transport-https ca-certificates gnupg
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+# sudo apt-get update -y && sudo apt-get install -y google-cloud-sdk
 
-sudo apt install -y imagemagick
+# sudo apt install -y imagemagick
 
-pip install gdown
+# pip install gdown
 mkdir -p pretrained_models
 
 # https://drive.google.com/uc?id=1UxUDbEygQ-CMBjRKACw_Xdj4RkDjirB5&confirm=t
-gdown '1UxUDbEygQ-CMBjRKACw_Xdj4RkDjirB5&confirm=t' -O ./pretrained_models/ # omnidata depth (v1)
-gdown '1Jrh-bRnJEjyMCS7f-WsaFlccfPjJPPHI&confirm=t' -O ./pretrained_models/ # omnidata depth (v2)
+# gdown '1UxUDbEygQ-CMBjRKACw_Xdj4RkDjirB5&confirm=t' -O ./pretrained_models/ # omnidata depth (v1)
+# gdown '1Jrh-bRnJEjyMCS7f-WsaFlccfPjJPPHI&confirm=t' -O ./pretrained_models/ # omnidata depth (v2)
+
+mkdir -p pretrained_models && cd pretrained_models
+wget 'https://zenodo.org/records/10447888/files/omnidata_dpt_depth_v1.ckpt?download=1'
+wget 'https://zenodo.org/records/10447888/files/omnidata_dpt_depth_v2.ckpt?download=1'
+cd ..
 
 
